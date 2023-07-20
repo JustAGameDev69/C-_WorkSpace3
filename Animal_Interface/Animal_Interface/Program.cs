096994b5e95d2de3f0a629994303e11ed7e33e40
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-20 20:56:29 +0700

    main

---
+++ Animal_Interface/Animal_Interface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AnkaraMessi
+{
+    public abstract class Animal
+    {
+        public abstract string MakeSound();
+    }
+
+    class Tiger : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Tiger sound: gruuuuuuhhhhhhh ";
+        }
+    }
+
+    class Chicken : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Chicken sound: toac toac toac ";
+        }
+
+    }
+
+    class Abstract_Interface
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals_1 = new Animal[2];
+
+            animals_1[1] = new Chicken();
+            animals_1[2] = new Tiger();
+
+            foreach (Animal animal in animals_1)
+            {
+                Console.WriteLine(animal.MakeSound());
+            }
+        }
+    }
+}
