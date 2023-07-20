bca96aa3d5aa0a48ae3420ed266b8fc75c0dbea7
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-20 21:22:51 +0700

    main

--- Animal_Interface/Animal_Interface/Program.cs
+++ Animal_Interface/Animal_Interface/Program.cs
@@ -15,13 +15,46 @@ namespace AnkaraMessi
         }
     }
 
-    class Chicken : Animal
+    class Chicken : Animal, Edible
     {
         public override string MakeSound()
         {
             return "Chicken sound: toac toac toac ";
         }
 
+        public string HowToEat()
+        {
+            return "Chicken could be fried!";
+        }
+    }
+
+    public abstract class Fruit : Edible
+    {
+
+        public abstract string HowToEat();
+    }
+
+    public class Apple: Fruit
+    {
+
+        public override string HowToEat()
+        {
+            return "Apple could be sliced!";
+        }
+    }
+
+    public class Orange : Fruit
+    {
+
+        public override string HowToEat()
+        {
+            return "Orange could be juiced!";
+        }
+    }
+
+    public interface Edible
+    {
+        public string HowToEat();
     }
 
     class Abstract_Interface
@@ -30,12 +63,27 @@ namespace AnkaraMessi
         {
             Animal[] animals_1 = new Animal[2];
 
+            animals_1[0] = new Tiger();
             animals_1[1] = new Chicken();
-            animals_1[2] = new Tiger();
 
             foreach (Animal animal in animals_1)
             {
                 Console.WriteLine(animal.MakeSound());
+
+                if (animal is Chicken)
+                {
+                    Edible edibler = (Chicken)animal;
+                    Console.WriteLine(edibler.HowToEat());
+                }
+            }
+
+            Fruit[] fruits = new Fruit[2];
+            fruits[0] = new Orange();
+            fruits[1] = new Apple();
+
+            foreach(Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit.HowToEat());
             }
         }
     }
